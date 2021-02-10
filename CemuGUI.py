aa41362d72be6ecfc6f2e1a0ba23f06b5538f412
mjcox244 <66311016+mjcox244@users.noreply.github.com>
2021-02-10 13:48:25 +0000

    Add the python file V1

---
+++ CemuGUI.py
@@ -0,0 +1,57 @@
+import os, wget, time
+from zipfile import ZipFile
+import shutil
+
+latest_cemu="1.22.5"
+try:
+	os.remove("CemuGUI-python-runner-main")
+	os.remove("cemu_" + latest_cemu)
+except:
+	print("haha perms go BRRRRR\n\n\n")
+	
+
+
+
+
+#get the latest version of this tool (at least when i work out how to copy the python file out the folder it makes)
+wget.download("https://github.com/mjcox244/CemuGUI-python-runner/archive/main.zip")
+with ZipFile('CemuGUI-python-runner-main.zip', 'r') as zipObj:
+	# Extract all the contents of zip file in current directory
+	zipObj.extractall()
+os.remove("CemuGUI-python-runner-main.zip")
+cmd = "xcopy CemuGUI-python-runner-main " + os.getcwd() + "/s /Y /Q"
+os.system(cmd)
+
+#update / dowmload cemu
+wget.download("https://cemu.info/releases/cemu_"+ latest_cemu +".zip")
+with ZipFile("cemu_"+ latest_cemu + ".zip", 'r') as zipObj:
+	# Extract all the contents of zip file in current directory
+	zipObj.extractall('')
+os.remove("cemu_"+ latest_cemu + ".zip")
+try:
+	os.system('cmd /c "mkdir Cemu"')
+except:
+	print("")
+os.system('cmd /c "xcopy cemu_1.22.5 Cemu /s /Y /Q"')
+time.sleep(1)
+
+
+
+
+#THIS IS THE GITHUB VERSION
+
+
+
+
+
+if(os.path.isfile("Setup-done")):
+	print ("DEBUG THE FILE IS THERE")
+else:
+	
+	#download CemuGUI
+	wget.download("https://download1653.mediafire.com/dl74cd2ahxmg/qfegbxxrda3i10r/CemuGUI+2.0+Beta.zip")
+	with ZipFile("CemuGUI 2.0 Beta.zip", 'r') as zipObj:
+		zipObj.extractall("")
+	print("so setup done isnt being writen then")
+	with open("Setup-done", 'w') as file_object:
+		file_object.write("Setup is done!")
